004fd66a0339a1fe78caf7255917c6fd950cac1d
IsaDhein <euisadhein@gmail.com>
2025-05-27 21:48:11 -0300

    continuação tk

--- tk/main.py
+++ tk/main.py
@@ -1,19 +1,75 @@
+# import tkinter as tk
+
+# def mostrar_opcao(botao):
+#     texto = f"Escolheu: {opcao1.get()}"
+#     texto += f" {opcao2.get()}"
+#     texto += f" {opcao3.get()}"
+#     rotulo.config(text= texto)
+
+#     op = [opcao1.get(), opcao2.get(), opcao3.get()]
+#     if op.count(True) == 3:
+#         if opcao1.get() and opcao2.get():
+#             if botao == "Saúde":
+#                 opcao1.set(False)
+#         if botao == "Tempo":
+#                 opcao3.set(False)
+#         if botao == "Dinheiro":
+#                 opcao2.set(False)
+    
+
+        
+# janela = tk.Tk()
+# janela.title("Exemplo Radiobutton")
+# janela.geometry("250x150")
+
+# opcao1 = tk.BooleanVar()
+# opcao2 = tk.BooleanVar()
+# opcao3 = tk.BooleanVar()
+# tk.Radiobutton(janela, text="Tempo", font=("Arial", 10), variable=opcao1, value="True", 
+#                        command=lambda:mostrar_opcao("Tempo")).pack()
+# tk.Radiobutton(janela, text="Dinheiro", font=("Arial", 10), variable=opcao2, value="True", 
+#                        command=lambda:mostrar_opcao("Dinheiro")).pack()
+# tk.Radiobutton(janela, text="Saúde", font=("Arial", 10), variable=opcao3, value="True", 
+#                        command=lambda:mostrar_opcao("Saúde")).pack()
+
+# rotulo = tk.Label(janela, text="Escolheu:", font=("Arial", 13))
+# rotulo.pack(pady=10)
+# janela.mainloop()
+
+
 import tkinter as tk
-def imprimirInfos():
-    global is_on
-    is_on = not is_on
-    if is_on: 
-        rotulo.config(text="Olá mundo!")
-    else:
-        rotulo.config(text="Olá turma!")
-is_on = False
 
 janela = tk.Tk()
-janela.title("Exemplo Botão")
-janela.geometry("600x400")
-rotulo = tk.Label(janela, text="Clique no botão abaixo" , 
-                          font=("Arial", 16))
+janela.title("Exemplo Radiobutton")
+janela.geometry("250x150")
+def mostrar_opcao(botao):
+    print(botao)
+    texto = f"Escolheu: {opcao1.get()}"
+    texto += f" {opcao2.get()}"
+    texto += f" {opcao3.get()}"
+    rotulo.config(text= texto)
+
+    op = [opcao1.get(), opcao2.get(), opcao3.get()]
+    if op.count(True) == 3:
+        if opcao1.get() and opcao2.get():
+            if botao == "Saúde":
+                opcao1.set(False)
+            if botao == "Tempo":
+                opcao3.set(False)
+            if botao == "Dinhero":
+                opcao2.set(False)
+            rotulo.config(text= f"Escolheu: {botao} e perdeu {texto})")
+    
+
+opcao1 = tk.BooleanVar()
+opcao2 = tk.BooleanVar()
+opcao3 = tk.BooleanVar()
+
+tk.Checkbutton(janela, text="Dinhero", variable=opcao1, command=lambda:mostrar_opcao("Dinhero")).pack()
+tk.Checkbutton(janela, text="Tempo", variable=opcao2, command=lambda:mostrar_opcao("Tempo")).pack()
+tk.Checkbutton(janela, text="Saúde", variable=opcao3, command=lambda:mostrar_opcao("Saúde")).pack()
+
+
+rotulo = tk.Label(janela, text="Escolheu: A")
 rotulo.pack(pady=10)
-botao = tk.Button(janela, text="Clique aqui", command=imprimirInfos)
-botao.pack(pady=10)
 janela.mainloop()
