b33513b9667deabe9be67f4d5bb926057f5cce9f
IsaDhein <euisadhein@gmail.com>
2025-05-28 21:08:50 -0300

    inicio entregavel

---
+++ regex/main.py
@@ -0,0 +1,11 @@
+import re
+
+# def validar_email(email):
+#     padrao = r'^[a-zA-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,2}$'
+#     return bool (re.match(padrao, email))
+
+# emails = ['usuario@exemplç.com', 'nome.sobrenome@empresa.com.br', 
+#          'invalido@', 'sem_arroba.com', '@dominio.com', 'guilherme.+_-p@ienh.com.br.asdf']
+
+# for email in emails:#para cada email dentro da lista emails
+#     print(f"{email}: {'Válido' if validar_email(email) else 'Inválido'}")
